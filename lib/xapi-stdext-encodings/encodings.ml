7e93506c4a6f786572444e3ec45f13736ee75e7d
Edwin Török <edwin.torok@cloud.com>
2023-05-16 15:00:42 +0100

    xapi-stdext-encodings: reimplement using Uutf
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- lib/xapi-stdext-encodings/encodings.ml
+++ lib/xapi-stdext-encodings/encodings.ml
@@ -20,75 +20,28 @@ exception UTF8_continuation_byte_invalid
 exception UTF8_encoding_not_canonical
 exception String_incomplete
 
-module Int = struct
-  include Int
-  let of_int (x:int) = x
-end
-
-type uchar = int
-
-(* === Utility Functions === *)
-
-let ( ||| ) = Int.logor
-let ( <<< ) = Int.shift_left
 
 (* === Unicode Functions === *)
 
 module UCS = struct
 
-  let min_value = 0x000000
-  let max_value = 0x1fffff
-
   let is_non_character value = false
                                || (0xfdd0 <= value && value <= 0xfdef) (* case 1 *)
                                || (Int.logand 0xfffe value = 0xfffe) (* case 2 *)
 
-  let is_out_of_range value =
-    value < min_value || value > max_value
-
-  let is_surrogate value =
-    (0xd800 <= value && value <= 0xdfff)
-
 end
 
-module XML = struct
+(* raises more detailed exception on what is wrong with a UTF8 character,
+   but not used during actual decoding *)
 
-  let is_forbidden_control_character value = value < 0x20
-                                             && value <> 0x09
-                                             && value <> 0x0a
-                                             && value <> 0x0d
-
-end
-
-(* === UCS Validators === *)
-
-module type UCS_VALIDATOR = sig
-
-  val validate : uchar -> unit
-
-end
-
-module UTF8_UCS_validator : UCS_VALIDATOR = struct
-
+module UCS_validator = struct
   let validate value =
-    if UCS.is_out_of_range  value then raise UCS_value_out_of_range;
-    if UCS.is_non_character value then raise UCS_value_prohibited_in_UTF8;
-    if UCS.is_surrogate     value then raise UCS_value_prohibited_in_UTF8
-
+    if Uchar.is_valid value then ()
+    else if value < Uchar.to_int Uchar.min || value > Uchar.to_int Uchar.max then raise UCS_value_out_of_range
+    else raise UCS_value_prohibited_in_UTF8
 end
 
-module XML_UTF8_UCS_validator : UCS_VALIDATOR = struct
-
-  let validate value =
-    UTF8_UCS_validator.validate value;
-    if XML.is_forbidden_control_character value
-    then raise UCS_value_prohibited_in_XML
-
-end
-
-(* ==== Character Codecs ==== *)
-
-module UTF8_CODEC (UCS_validator : UCS_VALIDATOR) = struct
+module UTF8_CODEC = struct
   (* === Decoding === *)
 
   let decode_header_byte byte =
@@ -106,27 +59,57 @@ module UTF8_CODEC (UCS_validator : UCS_VALIDATOR) = struct
     if value < 0x000080 (* 1 lsl  7 *) then 1 else
     if value < 0x000800 (* 1 lsl 11 *) then 2 else
     if value < 0x010000 (* 1 lsl 16 *) then 3 else 4
-
+    
   let decode_character string index =
-    let value, width = decode_header_byte (Char.code string.[index]) in
-    let value = if width = 1 then (Int.of_int value)
-      else begin
-        let value = ref (Int.of_int value) in
-        for index = index + 1 to index + width - 1 do
-          let chunk = decode_continuation_byte (Char.code string.[index]) in
-          value := (!value <<< 6) ||| (Int.of_int chunk)
-        done;
-        if width > (width_required_for_ucs_value !value)
-        then raise UTF8_encoding_not_canonical;
-        !value
-      end in
-    UCS_validator.validate value;
-    (value, width)
+      let value, width = decode_header_byte (Char.code string.[index]) in
+      let value = if width = 1 then value
+        else begin
+          let value = ref value in
+          for index = index + 1 to index + width - 1 do
+            let chunk = decode_continuation_byte (Char.code string.[index]) in
+            value := (!value lsl 6) lor chunk
+          done;
+          if width > (width_required_for_ucs_value !value)
+          then raise UTF8_encoding_not_canonical;
+          !value
+        end in
+      UCS_validator.validate value;
+      (value, width)
+end
+
+module XML = struct
+
+  let is_forbidden_control_character value = let value = Uchar.to_int value in
+                                             value < 0x20
+                                             && value <> 0x09
+                                             && value <> 0x0a
+                                             && value <> 0x0d
 
 end
 
-module     UTF8_codec = UTF8_CODEC (    UTF8_UCS_validator)
-module XML_UTF8_codec = UTF8_CODEC (XML_UTF8_UCS_validator)
+(* === UCS Validators === *)
+
+module type UCS_VALIDATOR = sig
+
+  val validate : Uchar.t -> unit
+
+end
+
+module UTF8_UCS_validator : UCS_VALIDATOR = struct
+
+  let validate value =
+    if UCS.is_non_character (Uchar.to_int value) then raise UCS_value_prohibited_in_UTF8
+
+end
+
+module XML_UTF8_UCS_validator : UCS_VALIDATOR = struct
+
+  let validate value =
+    UTF8_UCS_validator.validate value;
+    if XML.is_forbidden_control_character value
+    then raise UCS_value_prohibited_in_XML
+
+end
 
 (* === String Validators === *)
 
@@ -141,28 +124,41 @@ end
 exception Validation_error of int * exn
 
 module String_validator (Validator : UCS_VALIDATOR) : STRING_VALIDATOR = struct
-  include UTF8_CODEC(Validator)
-
   let validate string =
-    let index = ref 0 and length = String.length string in
-    begin try
-        while !index < length do
-          let _, width = decode_character string !index in
-          index := !index + width
-        done;
-      with
-      | Invalid_argument _ -> raise String_incomplete
-      | error -> raise (Validation_error (!index, error))
-    end; assert (!index = length)
+    let d = Uutf.decoder ~encoding:`UTF_8 (`String string) in
+    let rec loop () =
+      match Uutf.decode d with
+      | `Await ->
+          assert false (* we don't use Manual source *)
+      | `Uchar c ->
+          let () =
+            try Validator.validate c
+            with exn ->
+              raise (Validation_error (Uutf.decoder_byte_count d - 1, exn))
+          in
+          loop ()
+      | `End ->
+          ()
+      | `Malformed m -> (
+          let idx = Uutf.decoder_byte_count d - 1 in
+          try
+            let _, _ = UTF8_CODEC.decode_character m 0 in
+            ()
+          with
+          | Invalid_argument _ ->
+              raise String_incomplete
+          | e ->
+              raise (Validation_error (idx, e))
+        )
+    in
+    loop ()
 
-  let is_valid string =
-    try validate string; true with _ -> false
+  let is_valid string = try validate string ; true with _ -> false
 
   let longest_valid_prefix string =
-    try validate string; string
+    try validate string ; string
     with Validation_error (index, _) -> String.sub string 0 index
-
 end
 
-module UTF8     = String_validator (UTF8_UCS_validator)
+module UTF8 = String_validator (UTF8_UCS_validator)
 module UTF8_XML = String_validator (XML_UTF8_UCS_validator)
